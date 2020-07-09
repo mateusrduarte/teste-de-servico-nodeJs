7c716f5c346489c5015d679aafcd47c0c2e07cff
Mateus Duarte <mateusrduarte@gmail.com>
2020-07-08 21:42:06 -0300

    Criaçao do arquivo bancoDeDados.js e suas funcionalidades

--- src/servidor.js
+++ src/servidor.js
@@ -1,14 +1,24 @@
 const porta = 3003
 
 const express = require('express')
+const bancoDeDados = require('./bancoDeDados')
 
 const app = express()
 
 app.get('/produtos', (req, res, next) => {
-    res.send({
-        nome: 'Notebook',
-        preco: 123.45
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
     })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
